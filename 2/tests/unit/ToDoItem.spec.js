11631f82e1fc2ff16639e0b943a11ac36bdd8503
BeastyRiot <BeastyRiot@gmail.com>
2019-11-08 22:53:00 +0100

    all test working

--- 2/tests/unit/ToDoItem.spec.js
+++ 2/tests/unit/ToDoItem.spec.js
@@ -1,43 +1,41 @@
 import { shallowMount } from "@vue/test-utils";
-import { mount } from "@vue/test-utils";
 import ListItem from "@/components/ListItem.vue";
+
+const wrapper = shallowMount(ListItem, {
+  propsData: { 
+    todo: {
+        id: 1,
+        message: "todo.text"
+    }
+  }
+});
 
 describe("ListItem.vue", () => {
   it("renders todo.text", () => {
-    const wrapper = shallowMount(ListItem, {
-      propsData: { 
-        todo: {
-            id: 1,
-            message: "todo.text"
-        }
-      }
-    }); 
-
     expect(wrapper.html()).toContain('todo.text');
   })
   it("does not show input field", () => {
-    const wrapper = shallowMount(ListItem, {
-      propsData: { 
-        todo: {
-            id: 1,
-      message: ""
-        }
-      }
-    }); 
-
-    expect(wrapper.find('input').exists()).toBe(false);
+    expect(wrapper.find('input').exists()).toBe(false);    
   })
+  describe("click on 'Edit' button", () => {
+    it("show input field", () => {
+      wrapper.find('#edit').trigger('click')
 
-  it("shows input field", () => {
-    const wrapper = mount(ListItem, {
-      propsData: { 
-        todo: {
-            id: 1,
-      message: ""
-        }
-      }
-    }); 
+      expect(wrapper.find('input').exists()).toBe(true);
+    })
+    describe("edit text and submit", () => {
+      it("$emits 'save' with edited todo", () => {
+        wrapper.find('#save').trigger('click')
 
-    wrapper.find('#edit').trigger('click')
+        expect(wrapper.emitted('update-todo')).toBeTruthy()
+      })
+    })
+  })
+  describe("click on 'Delete' button", () => {
+    it("$emit 'delete'", () => {
+      wrapper.find('#delete').trigger('click')
+
+      expect(wrapper.emitted('delete-todo')).toBeTruthy()
+    })
   })
 });
