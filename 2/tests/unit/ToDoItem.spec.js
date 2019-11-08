49754f1a3723bb9fd5b0bb567d7155c00417745b
Stefan Jagdmann <sjagdmann10@googlemail.com>
2019-11-08 23:12:07 +0100

    added auto format & test build for penomatikus

--- 2/tests/unit/ToDoItem.spec.js
+++ 2/tests/unit/ToDoItem.spec.js
@@ -2,40 +2,40 @@ import { shallowMount } from "@vue/test-utils";
 import ListItem from "@/components/ListItem.vue";
 
 const wrapper = shallowMount(ListItem, {
-  propsData: { 
+  propsData: {
     todo: {
-        id: 1,
-        message: "todo.text"
+      id: 1,
+      message: "todo.text"
     }
   }
 });
 
 describe("ListItem.vue", () => {
   it("renders todo.text", () => {
-    expect(wrapper.html()).toContain('todo.text');
-  })
+    expect(wrapper.html()).toContain("todo.text");
+  });
   it("does not show input field", () => {
-    expect(wrapper.find('input').exists()).toBe(false);    
-  })
+    expect(wrapper.find("input").exists()).toBe(false);
+  });
   describe("click on 'Edit' button", () => {
     it("show input field", () => {
-      wrapper.find('#edit').trigger('click')
+      wrapper.find("#edit").trigger("click");
 
-      expect(wrapper.find('input').exists()).toBe(true);
-    })
+      expect(wrapper.find("input").exists()).toBe(true);
+    });
     describe("edit text and submit", () => {
       it("$emits 'save' with edited todo", () => {
-        wrapper.find('#save').trigger('click')
+        wrapper.find("#save").trigger("click");
 
-        expect(wrapper.emitted('update-todo')).toBeTruthy()
-      })
-    })
-  })
+        expect(wrapper.emitted("update-todo")).toBeTruthy();
+      });
+    });
+  });
   describe("click on 'Delete' button", () => {
     it("$emit 'delete'", () => {
-      wrapper.find('#delete').trigger('click')
+      wrapper.find("#delete").trigger("click");
 
-      expect(wrapper.emitted('delete-todo')).toBeTruthy()
-    })
-  })
+      expect(wrapper.emitted("delete-todo")).toBeTruthy();
+    });
+  });
 });
