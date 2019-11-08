6c30b213e6936ea9b77ebcaaf8f7a2794f728570
Asbach <andre.simon.amakunai@googlemail.com>
2019-11-08 23:05:25 +0100

    add auto build for travis ci

--- 2/jest.config.js
+++ 2/jest.config.js
@@ -23,10 +23,6 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/',
-  collectCoverage: true,
-  collectCoverageFrom: ['**/*.{vue}', '!**/node_modules/**', '!<rootDir>/dist/**',
-    '!<rootDir>/src/plugins/**', '!<rootDir>/tests/unit/**'
-  ],
-  coverageReporters: ['lcov', 'text-summary']
+  testURL: 'http://localhost/'
+
 }
