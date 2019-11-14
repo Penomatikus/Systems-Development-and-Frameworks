9051081641fd47069af2a66c818aa3f78c452aa6
Asbach <andre.simon.amakunai@googlemail.com>
2019-11-14 14:07:42 +0100

    prepare environment for ex3

---
+++ 3/src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+     <div id="list">
+       <list-item 
+                v-for="todo in todos"            
+                :key="todo.id"
+                :todo="todo"
+                @update-todo="updateTodo"
+                @delete-todo="deleteTodo">
+        </list-item>
+      </div>
+</template>
+
+<script>
+import ListItem from "./ListItem.vue";
+export default
+{   name: 'list',
+    props: ['todos'],
+    components: {
+        ListItem
+    },
+    methods: {
+        updateTodo: function(passedTodo) {
+            this.$emit('update-todo', passedTodo)
+        },  
+        deleteTodo: function(id) {
+            this.$emit('delete-todo', id)
+        }
+    }
+};
+</script>
