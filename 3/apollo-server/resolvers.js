e31224b4e4ee3d809ea2448f0630c6ea4362babe
Stefan Jagdmann <sjagdmann10@googlemail.com>
2019-11-20 14:41:01 +0100

    removed code from example to match with our schema

--- 3/apollo-server/resolvers.js
+++ 3/apollo-server/resolvers.js
@@ -1,72 +1,34 @@
-import GraphQLJSON from 'graphql-type-json'
-import shortid from 'shortid'
-
+import GraphQLJSON from "graphql-type-json";
+import shortid from "shortid";
 
 export default {
   JSON: GraphQLJSON,
 
-  Counter: {
-    countStr: counter => `Current count: ${counter.count}`,
-  },
-
-
   Query: {
-    hello: (root, { name }) => `Hello ${name || 'World'}!`,
-    messages: (root, args, { db }) => db.get('messages').value(),
-    uploads: (root, args, { db }) => db.get('uploads').value(),
-
+    hello: (root, { name }) => `Hello ${name || "World"}!`,
+    todos: (root, args, { db }) => db.get("todos").value()
   },
 
   Mutation: {
     myMutation: (root, args, context) => {
-      const message = 'My mutation completed!'
-      context.pubsub.publish('hey', { mySub: message })
-      return message
+      const message = "My mutation completed!";
+      context.pubsub.publish("hey", { mySub: message });
+      return message;
     },
-    addMessage: (root, { input }, { pubsub, db }) => {
-      const message = {
+    addTodo: (root, { input }, { pubsub, db }) => {
+      const todo = {
         id: shortid.generate(),
-        text: input.text,
-      }
+        message: input.text
+      };
 
-      db
-        .get('messages')
-        .push(message)
+      db.get("todos")
+        .push(todo)
         .last()
-        .write()
+        .write();
 
-      pubsub.publish('messages', { messageAdded: message })
+      pubsub.publish("todos", { todoAdded: todo });
 
-      return message
-    },
-
-    singleUpload: (root, { file }, { processUpload }) => processUpload(file),
-    multipleUpload: (root, { files }, { processUpload }) => Promise.all(files.map(processUpload)),
-
-  },
-
-  Subscription: {
-    mySub: {
-      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('hey'),
-    },
-    counter: {
-      subscribe: (parent, args, { pubsub }) => {
-        const channel = Math.random().toString(36).substring(2, 15) // random channel name
-        let count = 0
-        setInterval(() => pubsub.publish(
-          channel,
-          {
-            // eslint-disable-next-line no-plusplus
-            counter: { count: count++ },
-          }
-        ), 2000)
-        return pubsub.asyncIterator(channel)
-      },
-    },
-
-    messageAdded: {
-      subscribe: (parent, args, { pubsub }) => pubsub.asyncIterator('messages'),
-    },
-
-  },
-}
+      return todo;
+    }
+  }
+};
