a9430dc05885931d2fcc8ec2f145febef549c485
Stefan Jagdmann <sjagdmann10@googlemail.com>
2019-12-09 19:33:36 +0100

    some new configurations

--- 4/.eslintrc.js
+++ 4/.eslintrc.js
@@ -1,32 +1,39 @@
 module.exports = {
-  root: true,
+    root: true,
 
-  env: {
-    node: true
-  },
+    env: {
+        node: true,
+    },
 
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+    extends: [
+        'plugin:vue/essential',
+        '@vue/prettier',
+        'plugin:prettier/recommended',
+    ],
 
-  rules: {
-    "no-console": "off",
-    "no-debugger": "off"
-  },
+    rules: {
+        'no-console': 'off',
+        'no-debugger': 'off',
+    },
 
-  parserOptions: {
-    parser: "babel-eslint"
-  },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
 
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ],
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 
-  plugins: ["graphql"]
-};
+    plugins: ['graphql', 'prettier'],
+    rules: {
+        prettier: ['error'],
+    },
+}
