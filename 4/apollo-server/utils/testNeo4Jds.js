164d84414ea5eb4d2f1ad5ebcf594c371218bced
Stefan Jagdmann <sjagdmann10@googlemail.com>
2019-12-09 19:32:15 +0100

    used prittier to format code

--- 4/apollo-server/utils/testNeo4Jds.js
+++ 4/apollo-server/utils/testNeo4Jds.js
@@ -1,136 +1,138 @@
-import { driver } from 'neo4j-driver';
-import { async } from 'rxjs/internal/scheduler/async';
+import { driver } from 'neo4j-driver'
+import { async } from 'rxjs/internal/scheduler/async'
 var neo4j = require('neo4j-driver')
 
-const { DataSource } = require('apollo-datasource');
+const { DataSource } = require('apollo-datasource')
 
-function searchelement(mok, id){
-for (var i=0;i<mok.length;i++) {
-  if (mok[i].id == id) 
-  {
-    return mok[i]
-  }
-}
-return "findTodo: element not found"
+function searchelement(mok, id) {
+    for (var i = 0; i < mok.length; i++) {
+        if (mok[i].id == id) {
+            return mok[i]
+        }
+    }
+    return 'findTodo: element not found'
 }
 
 function filterTodos(mok, userAuth) {
-  var retArray = [];
+    var retArray = []
 
-  for (var i=0;i<mok.length;i++) {
-    if (mok[i].userAuth == userAuth) 
-    {      
-      retArray.push(mok[i])
+    for (var i = 0; i < mok.length; i++) {
+        if (mok[i].userAuth == userAuth) {
+            retArray.push(mok[i])
+        }
     }
-  }
 
-  if(retArray.length == 0)
-    console.log("no valid todos found for user")
-  return retArray
+    if (retArray.length == 0) console.log('no valid todos found for user')
+    return retArray
 }
 
 // this.store => a neo4j driver of database
 export class TodoNeo4JAPI extends DataSource {
-  
-  constructor( store ) {
-    super();
-    this.store = store;
-  }
-
-  initialize(config) {
-    this.context = config.context;
-  }
-
-  nodeToTodoObject(foundNode){
-    return {
-      id: foundNode.properties.id.low, 
-      message: foundNode.properties.message, 
-      userAuth: foundNode.properties.userAuth
+    constructor(store) {
+        super()
+        this.store = store
     }
-  }
-  
-  async findTodo(id, userAuth) {
-    const driver = this.store
-    const session = driver.session()     
-    let foundTodo;    
 
-    try {
-      await session.run(`MATCH (todo:Todo { id: ${id} }) return todo`)
-        .then(result => { 
-          const node = result.records[0].get("todo"); 
-          foundTodo = this.nodeToTodoObject(node)
-        })
-        .catch(error => { console.log(error)})
-    } catch (error) {
-      console.log(error)
-    } finally {
-      session.close()
+    initialize(config) {
+        this.context = config.context
     }
-    return foundTodo
-  }
 
-  getAllTodos() {
-    var session = this.store.session()
-
-    let allTodos;
-
-    session.run('MATCH (n) return n', {})
-    .then(result => {      
-      console.log(result.records)
-      allTodos = result.records;      
-    })
-    .catch(error => {
-      console.log(error)
-    })
-    .then(() => session.close())
-  
-
-    return allTodos;
-  }
-
-  getTodosForUser(userAuth) {
-    return filterTodos(this.store, userAuth);
-  }
-
-  deleteTodo(id) {
-   return this.store.splice( this.store.indexOf({id: id}), 1 );
-  }
- 
-  async addTodo(todo, userAuth) {
-    const driver = this.store  
-    const session = driver.session()
-
-    const cypher = `CREATE (n:Todo { id: ${todo.id}, message: '${todo.message}', userAuth: '${userAuth}' })`;
-    try {
-      await session.run(cypher).catch(e => { console.log(e); })
-    } catch(error) { 
-      console.log("ERROR: " + error)
-    } finally {
-        session.close()
+    nodeToTodoObject(foundNode) {
+        return {
+            id: foundNode.properties.id.low,
+            message: foundNode.properties.message,
+            userAuth: foundNode.properties.userAuth,
+        }
     }
-  }
 
-  async updateTodo(id, newmessage){
-    const driver = this.store
-    const session = driver.session()
-    let updatedTodo;
+    async findTodo(id, userAuth) {
+        const driver = this.store
+        const session = driver.session()
+        let foundTodo
 
-    const cypher = `MATCH (todo:Todo {id: ${id}}) SET todo.message = '${newmessage}' return todo as todo`
-    try {
-      await session.run(cypher).then(result => { 
-        const node = result.records[0].get("todo"); 
-        updatedTodo = this.nodeToTodoObject(node)
-      })
-      .catch(error => { console.log(error)})
-    } catch(error) { 
-      console.log("ERROR: " + error)
-    } finally {
-        session.close()
+        try {
+            await session
+                .run(`MATCH (todo:Todo { id: ${id} }) return todo`)
+                .then(result => {
+                    const node = result.records[0].get('todo')
+                    foundTodo = this.nodeToTodoObject(node)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        } catch (error) {
+            console.log(error)
+        } finally {
+            session.close()
+        }
+        return foundTodo
     }
-   return updatedTodo
-  }
 
+    getAllTodos() {
+        var session = this.store.session()
+
+        let allTodos
+
+        session
+            .run('MATCH (n) return n', {})
+            .then(result => {
+                console.log(result.records)
+                allTodos = result.records
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            .then(() => session.close())
+
+        return allTodos
+    }
+
+    getTodosForUser(userAuth) {
+        return filterTodos(this.store, userAuth)
+    }
+
+    deleteTodo(id) {
+        return this.store.splice(this.store.indexOf({ id: id }), 1)
+    }
+
+    async addTodo(todo, userAuth) {
+        const driver = this.store
+        const session = driver.session()
+        const cypher = `CREATE (n:Todo { id: ${todo.id}, message: '${todo.message}', userAuth: '${userAuth}' })`
+        try {
+            await session.run(cypher).catch(e => {
+                console.log(e)
+            })
+        } catch (error) {
+            console.log('ERROR: ' + error)
+        } finally {
+            session.close()
+        }
+    }
+
+    async updateTodo(id, newmessage) {
+        const driver = this.store
+        const session = driver.session()
+        let updatedTodo
+
+        const cypher = `MATCH (todo:Todo {id: ${id}}) SET todo.message = '${newmessage}' return todo as todo`
+        try {
+            await session
+                .run(cypher)
+                .then(result => {
+                    const node = result.records[0].get('todo')
+                    updatedTodo = this.nodeToTodoObject(node)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        } catch (error) {
+            console.log('ERROR: ' + error)
+        } finally {
+            session.close()
+        }
+        return updatedTodo
+    }
 }
 
 export const ds = new TodoNeo4JAPI()
-
