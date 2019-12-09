f8722871672ef6293145f8c7dfb24d52814de1b0
Stefan Jagdmann <sjagdmann10@googlemail.com>
2019-12-09 21:12:49 +0100

    some changes mostly done by eslint & prettier

--- 4/.eslintrc.js
+++ 4/.eslintrc.js
@@ -1,39 +1,36 @@
 module.exports = {
-    root: true,
+	root: true,
 
-    env: {
-        node: true,
-    },
+	env: {
+		node: true,
+	},
 
-    extends: [
-        'plugin:vue/essential',
-        '@vue/prettier',
-        'plugin:prettier/recommended',
-    ],
+	extends: [
+		'plugin:vue/essential',
+		'@vue/prettier',
+		'plugin:prettier/recommended',
+	],
 
-    rules: {
-        'no-console': 'off',
-        'no-debugger': 'off',
-    },
+	rules: {
+		'no-console': 'off',
+		'no-debugger': 'off',
+	},
 
-    parserOptions: {
-        parser: 'babel-eslint',
-    },
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
 
-    overrides: [
-        {
-            files: [
-                '**/__tests__/*.{j,t}s?(x)',
-                '**/tests/unit/**/*.spec.{j,t}s?(x)',
-            ],
-            env: {
-                jest: true,
-            },
-        },
-    ],
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
 
-    plugins: ['graphql', 'prettier'],
-    rules: {
-        prettier: ['error'],
-    },
+	plugins: ['graphql', 'prettier'],
 }
